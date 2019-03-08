275d025aa66c6ec5ea885396159709612b795c09
BostjanStupica <bostjan@stupica.com>
2019-03-08 20:50:06 +0100

    Git/Project reorganization

---
+++ 11helloWorldC/11helloWorldC/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  11helloWorldC
+//
+//  Created by Bostjan Stupica on 08/03/2019.
+//  Copyright © 2019 stupica.com. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
